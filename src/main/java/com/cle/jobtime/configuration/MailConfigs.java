13797433d1da4711caaa21e7b1575593e93ffff6
C L <C L@Avados-lenovo>
2017-06-19 22:27:37 -0400

    pwd

--- src/main/java/com/cle/jobtime/configuration/MailConfigs.java
+++ src/main/java/com/cle/jobtime/configuration/MailConfigs.java
@@ -10,7 +10,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration 
-public class MailConfig {
+public class MailConfigs {
 
     @Autowired
     private Environment env;
